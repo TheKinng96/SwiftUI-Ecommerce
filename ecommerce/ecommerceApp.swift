b09ec2261dbf2e3089c9b0015418fb25eca21643
G3N <fengyuan.yap@gmail.com>
2022-07-10 21:49:23 +0900

    Initial Commit

---
+++ ecommerce/ecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceApp.swift
+//  ecommerce
+//
+//  Created by Feng Yuan Yap on 2022/07/10.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
